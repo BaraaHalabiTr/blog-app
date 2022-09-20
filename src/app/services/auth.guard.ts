5d27810614969926de8353fa45534caf0bad1398
BaraaHalabiTr <94146567+BaraaHalabiTr@users.noreply.github.com>
2022-09-20 03:15:55 +0300

    Added Login Route with Guard

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, take, tap, map } from 'rxjs';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(next: any, state: any): Observable<boolean> {
+    return this.auth.user$.pipe(
+      take(1),
+      map(user => !!user),
+      tap(signedIn => {
+        if(signedIn)
+          this.router.navigate(['/home']);
+        this.router.navigate(['/login']);
+      }) 
+    )
+  }
+}
